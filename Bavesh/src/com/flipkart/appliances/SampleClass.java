e29b12fbf728d2d52981ecc6b7faf4ae1d436885
Lenovo <Lenovo@Lenovo-PC>
2021-01-06 09:27:51 +0530

    addng new class

--- Bavesh/src/com/flipkart/appliances/SampleClass.java
+++ Bavesh/src/com/flipkart/appliances/SampleClass.java
@@ -1,17 +1,12 @@
 package com.flipkart.appliances;
 
 public class SampleClass {
-	
-	int a=100;
-	
-	void m1(){
-		System.out.println("Welcome to Java Traing classes");
-		System.out.println(a);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HEllos");
+
 	}
 
-	public static void main(String[] args){
-		SampleClass sample=new SampleClass();
-	    sample.m1();
-	   System.out.println(sample.a);
-	}
 }
