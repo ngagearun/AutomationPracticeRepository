d9e7b60df227ec7bdbb5d00121eb887249c4d3c5
bavesh0009 <bavesh9@gmail.com>
2021-01-09 20:27:43 -0500

    Second code Review

--- Bavesh/src/com/flipkart/appliances/EmployeeDetails.java
+++ Bavesh/src/com/flipkart/appliances/EmployeeDetails.java
@@ -5,12 +5,8 @@ public class EmployeeDetails {
 	String EmployeeName;
 	int EmployeeID;
 	String Employeeemailid;
-	 int  EmployeeOrganization;
+	static String  EmployeeOrganization="abc";
 	
-	static void Method1() {
-		
-		
-	}
 	
 	
 
@@ -20,18 +16,17 @@ public class EmployeeDetails {
 		EmployeeDetails e1= new EmployeeDetails();
 		e1.EmployeeName="Bavesh";
 		e1.Employeeemailid="Bavesh23@gmail.com";
-		Method1();
 		System.out.println("Name: " + e1.EmployeeName);
         System.out.println("Emailid:" + e1.Employeeemailid);
-		System.out.println("abc");
+		System.out.println("orgaization:" +e1.EmployeeOrganization );
+		
 		
 		EmployeeDetails e2= new EmployeeDetails();
 		e2.EmployeeName="ram";
 		e2.Employeeemailid="ram23@gmail.com";
-		Method1();
 		System.out.println("Name: " + e2.EmployeeName);
         System.out.println("Emailid:" + e2.Employeeemailid);
-		System.out.println("xyz");
+        System.out.println("organization:" +e2.EmployeeOrganization);
 		
 	}
 	
