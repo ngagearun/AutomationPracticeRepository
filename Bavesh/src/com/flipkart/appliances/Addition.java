b5fcf79407e2332c97960920d392f0804606811d
Jhilly <Jhilly@LAPTOP-EELKOJ1U.fios-router.home>
2021-01-09 22:41:56 -0500

    Adding new file

--- Bavesh/src/com/flipkart/appliances/Addition.java
+++ Bavesh/src/com/flipkart/appliances/Addition.java
@@ -1,5 +1,20 @@
 package com.flipkart.appliances;
 
+
+
 public class Addition {
+	void add(int a,int b,int c) {
+		
+		int sum=a+b+c;
+		System.out.println(sum);
+	}
+
+	public static void main(String[] args) {
+		Addition a= new Addition();
+		a.add(100, 200, 300);
+		a.add(400, 500, 600);
+		a.add(700, 800, 900);
+
+	}
 
 }
