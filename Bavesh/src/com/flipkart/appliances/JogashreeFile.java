132e63ae16a20dfbd141b42b21eacfe1aa7f3819
Jhilly <Jhilly@LAPTOP-EELKOJ1U.fios-router.home>
2021-01-26 17:32:50 -0500

    Adding changes

--- Bavesh/src/com/flipkart/appliances/JogashreeFile.java
+++ Bavesh/src/com/flipkart/appliances/JogashreeFile.java
@@ -3,7 +3,7 @@ package com.flipkart.appliances;
 public class JogashreeFile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("New File");
 
 	}
 
