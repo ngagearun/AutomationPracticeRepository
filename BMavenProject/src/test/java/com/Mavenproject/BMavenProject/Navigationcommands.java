5f8d01c74c525dd63073a0057328d010c435b4a7
bavesh0009 <bavesh9@gmail.com>
2021-02-05 10:41:07 -0500

    Selinium Second checkin -TSC002

---
+++ BMavenProject/src/test/java/com/Mavenproject/BMavenProject/Navigationcommands.java
@@ -0,0 +1,31 @@
+package com.Mavenproject.BMavenProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByLinkText;
+
+public class Navigationcommands extends Selinium {
+
+	public static void main(String[] args) throws InterruptedException {
+		launchbrowser();
+		openapp("https://www.selenium.dev/");
+		Thread.sleep(3000);
+		click(By.linkText("Downloads"));
+		click(By.linkText("Projects"));
+		click(By.linkText("Documentation"));
+		driver.navigate().back();
+		driver.navigate().forward();
+		
+		search(By.id("gsc-i-id1"),("selinium"));
+		driver.close();
+	}
+	
+
+	
+
+
+	
+
+
+	}
+	
+
