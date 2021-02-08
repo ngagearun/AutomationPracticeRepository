9546b297a2d979b911c5279af6f8f1f5485659ae
bavesh0009 <bavesh9@gmail.com>
2021-02-08 12:35:38 -0500

    Selinium Script - TSC004

---
+++ Bavesh/src/com/flipkart/appliances/Selinium.java
@@ -0,0 +1,33 @@
+package com.flipkart.appliances;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Selinium {
+static EdgeDriver driver;
+	
+	
+	public static void launchbrowser() {
+		System.setProperty("webdriver.edge.driver","C:\\Users\\baves\\Desktop\\msedgedriver.exe");
+		driver= new EdgeDriver();
+		
+	}
+	public static void openapp(String url) throws InterruptedException{
+		driver.get(url);
+		Thread.sleep(3000);
+
+	}
+	
+
+  public static void click(By Loacatorname) {
+	  
+	  driver.findElement(Loacatorname).click();
+  }
+  
+  public static void search(By locatorname, String value) {
+	    driver.findElement(locatorname).clear();
+	    driver.findElement(locatorname).sendKeys(value);
+	  
+  }
+}
+
